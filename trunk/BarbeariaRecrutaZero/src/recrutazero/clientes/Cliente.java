f2c726c0b94e84946b9b6ab6eaad6222c6f4daa2
fe.atavares <fe.atavares@3eca69ca-d45e-11de-b7d7-53e004a27ad6>
2009-12-04 19:43:24 +0000

    Proporcionalidade funcionando

--- trunk/BarbeariaRecrutaZero/src/recrutazero/clientes/Cliente.java
+++ trunk/BarbeariaRecrutaZero/src/recrutazero/clientes/Cliente.java
@@ -3,14 +3,17 @@ package recrutazero.clientes;
 public class Cliente {
 	private int tempoServico;
 	private EnumPatente patente;
-
+	protected int id;
+	
 	public Cliente(EnumPatente patente) {
 		this.patente = patente;
+		this.id = gerarId();
 	}
 	
 	public Cliente(EnumPatente patente, int tempoServico) {
 		this.patente = patente;
 		this.tempoServico = tempoServico;
+		this.id = gerarId();
 	}
 
 	public int getTempoServico() {
@@ -33,5 +36,36 @@ public class Cliente {
 	public String toString() {
 		return patente.getPatente() + "\t" + tempoServico;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	private int gerarId() {
+		Long tempoCorrente = System.currentTimeMillis();
+		
+		String stringTempo = Long.toString(tempoCorrente);
+		
+		this.id = Integer.parseInt(stringTempo.substring(4, 13));
+		
+		return this.id;
+	}
+	
+	@Override
+	public boolean equals(Object c) {
+		if (c instanceof Cliente) {
+			Cliente cliente = (Cliente) c;
+			return this.getId().equals(cliente.getId());
+		}
+		else {
+			return false;
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
 
 }
