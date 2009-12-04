10513f779e384ecc874f2cdca3b4b0b05f22c5ac
fe.atavares <fe.atavares@3eca69ca-d45e-11de-b7d7-53e004a27ad6>
2009-12-04 19:43:24 +0000

    Proporcionalidade funcionando

--- BarbeariaRecrutaZero/src/recrutazero/forte/Forte.java
+++ BarbeariaRecrutaZero/src/recrutazero/forte/Forte.java
@@ -1,12 +1,9 @@
 package recrutazero.forte;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 import recrutazero.clientes.Cliente;
 import recrutazero.clientes.EnumPatente;
@@ -17,11 +14,6 @@ public class Forte {
 	protected Iterator<Cliente> iterator;
 	private static final int MAX_CLIENTES = 1000;
 	
-	private LinkedList<Cliente> listaClienteOficial;
-	private LinkedList<Cliente> listaClienteSargento;
-	private LinkedList<Cliente> listaClientePraca;
-	
-
 	public Forte(GeradorEventos geradorEventos) {
 		this.geradorEventos = geradorEventos;
 
@@ -29,92 +21,85 @@ public class Forte {
 	}
 
 	protected List<Cliente> geraListaClientes() {
-		List<Cliente> clientes = new ArrayList<Cliente>();
+		
+		System.out.println("Gerando uma lista aleatoria de clientes: 10% oficiais, 20% sargentos, 70% praças");
 
 		// Cria um número aleatório de pausas. No máximo 10% dos clientes pode ser pausa
 		int numeroPausas = (int)(MAX_CLIENTES * 0.1);
 		
 		int numClientes = MAX_CLIENTES - numeroPausas;
-		
-		// Popula lista de Oficiais, Sargentos e Praças
-		popularListaClientes(numClientes);
-		
 
-		// Cria uma lista com as posicoes que as pausas terão.
-		List<Integer> listaPausas = criarListaPausas(numeroPausas);
+		HashSet<Cliente> hashCliente = geraHashClientes(numeroPausas, numClientes);
 		
-		Iterator<Integer> iterator = listaPausas.iterator();
+		Cliente [] clientes = new Cliente[hashCliente.size()];
+		clientes = hashCliente.toArray(clientes);
 		
-		// Pega a primeira pausa
-		int proximaPausa = 0;
-		if (iterator.hasNext()) {
-			proximaPausa = iterator.next();
+		List<Cliente> listaClientes = new ArrayList<Cliente>();
+		
+		for (Cliente cliente : clientes) {
+			listaClientes.add(cliente);
 		}
-
-		for (int i = 0; i < MAX_CLIENTES; i++) {
-			Cliente cliente = null;
-			
-			if (i != proximaPausa) {
-				cliente = escolheTipoCliente();
-			}
-			else {
-				cliente = new Cliente(EnumPatente.PAUSA);
-				
-				// Vê quando vai ser a próxima pausa
-				if (iterator.hasNext()) {
-					proximaPausa = iterator.next();
-				}
-			}
-
-
-			clientes.add(cliente);
-		}
-
-		return clientes;
+		
+		return listaClientes;
 	}
 
-	private void popularListaClientes(int numClientes) {
-		listaClienteOficial = new LinkedList<Cliente>();
-		for (int i = 0; i < (int)(numClientes * 0.1); i++) {
-			EnumPatente patente = EnumPatente.OFICIAL;
-			int baseTempoServico = 4;
-			int offSizeTempoServico = geraEvento() % 3; // Variando entre 0 e 2
-			
-			listaClienteOficial.add(new Cliente(patente, baseTempoServico + offSizeTempoServico));
-		}
+	private HashSet<Cliente> geraHashClientes(int numeroPausas, int numClientes) {
+		HashSet<Cliente> hashCliente = new HashSet<Cliente>();
 		
-		listaClienteSargento = new LinkedList<Cliente>();
-		for (int i = 0; i < (int)(numClientes * 0.2); i++) {
-			EnumPatente patente = EnumPatente.SARGENTO;
-			int baseTempoServico = 2;
-			int offSizeTempoServico = geraEvento() % 3; // Variando entre 0 e 2
-			
-			listaClienteSargento.add(new Cliente(patente, baseTempoServico + offSizeTempoServico));
-		}
-		
-		listaClientePraca = new LinkedList<Cliente>();
-		for (int i = 0; i < (int)(numClientes * 0.7); i++) {
-			EnumPatente patente = EnumPatente.PRACA;
-			int baseTempoServico = 1;
-			int offSizeTempoServico = geraEvento() % 3; // Variando entre 0 e 2
-			
-			listaClientePraca.add(new Cliente(patente, baseTempoServico + offSizeTempoServico));
-		}
-		
-	}
-
-	private List<Integer> criarListaPausas(int numeroPausas) {
-		List<Integer> listaPausas = new ArrayList<Integer>();
 		for (int i = 0; i < numeroPausas; ) {
-			int proxima = geraEvento() % MAX_CLIENTES;
-			if (!listaPausas.contains(proxima)) {
-				listaPausas.add(proxima);
+			if (hashCliente.add(new Cliente(EnumPatente.PAUSA))) {
 				i++;
 			}
 		}
-		Collections.sort(listaPausas);
 		
-		return listaPausas;
+		for (int i = 0; i < Math.round(numClientes * 0.1);) {
+			EnumPatente patente = EnumPatente.OFICIAL;
+			if (hashCliente.add(new Cliente(patente, geraTempoServico(patente)))) {
+				i++;
+			}
+		}
+		
+		for (int i = 0; i < Math.round(numClientes * 0.2);) {
+			EnumPatente patente = EnumPatente.SARGENTO;
+			if (hashCliente.add(new Cliente(patente, geraTempoServico(patente)))) {
+				i++;
+			}
+		}
+		
+		for (int i = 0; i < Math.round(numClientes * 0.7);) {
+			EnumPatente patente = EnumPatente.PRACA;
+			if (hashCliente.add(new Cliente(patente, geraTempoServico(patente)))) {
+				i++;
+			}
+		}
+		return hashCliente;
+	}
+	
+	private int geraTempoServico(EnumPatente patente) {
+		
+		int tempoMinimo;
+		int tempoAdicional;
+		int tempoTotalServico = 0;
+		
+		switch (patente) {
+		case OFICIAL:
+			tempoMinimo = 4;
+			tempoAdicional = geraEvento() % 3;
+			tempoTotalServico = tempoMinimo + tempoAdicional;
+			break;
+		case SARGENTO:
+			tempoMinimo = 2;
+			tempoAdicional = geraEvento() % 3;
+			tempoTotalServico = tempoMinimo + tempoAdicional;
+			break;
+		case PRACA:
+			tempoMinimo = 1;
+			tempoAdicional = geraEvento() % 3;
+			tempoTotalServico = tempoMinimo + tempoAdicional;
+			break;
+		}
+		
+		return tempoTotalServico;
 	}
 
 	/**
@@ -137,66 +122,5 @@ public class Forte {
 		}
 		return cliente;
 	}
-
-	private Cliente escolheTipoCliente() {
-		
-		Cliente cliente = null;
-		boolean escolhido = false;
-
-		while (!escolhido) {
-			int numeroLista = geraEvento() % 3;
-
-			switch (numeroLista) {
-			case 0:
-				if (!listaClienteOficial.isEmpty()) {
-					cliente = listaClienteOficial.remove();
-					escolhido = true;
-				}
-				break;
-				
-			case 1:
-				if (!listaClienteSargento.isEmpty()) {
-					cliente = listaClienteSargento.remove();
-					escolhido = true;
-				}
-				break;
-			case 2:
-				if (!listaClientePraca.isEmpty()) {
-					cliente = listaClientePraca.remove();
-					escolhido = true;
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		
-		
-		
-
-//		EnumPatente patente;
-//		int tipoCliente = geraEvento() % 100;
-//		int offSizeTempoServico = geraEvento() % 3; // Variando entre 0 e 2
-//		int baseTempoServico = 0;
-//
-//		if (tipoCliente <= 70) {
-//			patente = EnumPatente.PRACA;
-//			baseTempoServico = 1;
-//		} else {
-//
-//			if (tipoCliente <= 90) {
-//				patente = EnumPatente.SARGENTO;
-//				baseTempoServico = 2;
-//			} else {
-//				patente = EnumPatente.OFICIAL;
-//				baseTempoServico = 4;
-//			}
-//		}
-//
-//		Cliente cliente = new Cliente(patente);
-//		cliente.setTempoServico(baseTempoServico + offSizeTempoServico);
-		return cliente;
-	}
-	
 
 }
